1918ed749cfaac5e6eab178f97141b9a0e64bb87
Aman Tiwari <amantiwari@gmail.com>
2026-04-17 09:31:36 +0530

    abstraction

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        Dog d=new Dog();
+        d.sound();
+
+    }
+}
+abstract class Animal{
+    abstract void sound();
+
+    }
+
+    class Dog extends Animal{
+    void sound(){
+        System.out.println("dog barks");
+    }
+    }
+
